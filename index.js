4cf780eeec73464c7597dd36d274da2096e7a4cb
zchristy <zchristy44@gmail.com>
2019-06-10 15:28:25 -0500

    Complete guided Project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs')
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -17,6 +18,10 @@ server.get('/', (req, res) => {
 
 server.post('/api/register', (req, res) => {
   let user = req.body;
+  //hash the password
+  const hash = bcrypt.hashSync(user.password, 8) //password gets re-hashed 2 ^8 times
+
+  user.password = hash
 
   Users.add(user)
     .then(saved => {
@@ -27,13 +32,13 @@ server.post('/api/register', (req, res) => {
     });
 });
 
-server.post('/api/login', (req, res) => {
+server.post('/api/login',  (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -44,13 +49,35 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function restricted(req, res, next) {
+  // read username and password from the headers and verify them
+  const{ username, password } = req.headers
+
+  if(username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(401).json({message: "Invalid Credentials"})
+  }
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
---
+++ notes.md
@@ -0,0 +1,29 @@
+
+
+code smell,  ---> Anti-Patterns
+
+--Some Patterns--
+
+- Model View Controller
+- Model Veiw Presenter
+- Builder
+- Factory
+- Repository
+
+Authentication (AuthN): who are you?
+Authorization (AuthZ): what can you do?
+
+In the future, learn about this if it seems fun to you.
+
+OAuth2 (authz) + OpenId Connect (OIDC)
+
+Identity Server -- token -- Resource Server
+
+Heroku(clients) request access to the resource
+
+the owner of the resource needs to approve access and grant permission (provides a token)
+
+Github (Resources)
+
+**always user https**
+**use as many rounds as you can get away with**
